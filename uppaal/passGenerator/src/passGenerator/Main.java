7b494f9f07e8c28b0d3489edfdb150d9cb8ed0eb
Finn Jannsen <finnjannsen@yahoo.de>
2019-11-25 20:31:00 +0100

    update doc

--- uppaal/passGenerator/src/passGenerator/Main.java
+++ uppaal/passGenerator/src/passGenerator/Main.java
@@ -5,46 +5,49 @@ import java.util.Random;
 public class Main {
 	public static void main(String[] args) {
 		Random zufall = new Random();
-		int noPass = 20;
-		if (args.equals("")) {
-			noPass = Integer.parseInt(args[0]);
-		}
+		int noPass = 10;
+
+		System.out.println("System declaration");
 		for (int i=0; i<noPass; i++) {
-			System.out.println("Pass" + i + " = Passenger_s(" + i + "," + zufall.nextInt(5) + "," + zufall.nextInt(5) + "," + zufall.nextInt(400) + ");");
+			System.out.println("Pass" + i + " = Passenger_s(" + i + "," + zufall.nextInt(5) + "," + zufall.nextInt(5) + "," + zufall.nextInt(100) + ");");
 		}
 		System.out.print("system Taxi1,Serv1,Serv2");
 		for (int i=0; i<noPass; i++) {
 			System.out.print(",Pass" + i);
 		}
-		System.out.print(";\n");
+		System.out.print(";\n\n");
 
-		System.out.print("E<> Pass0.WaitTimeout");
+		System.out.println("Ziel 1");
+		System.out.print("A[] not (Pass0.WaitTimeout");
 		for (int i=1; i<noPass; i++) {
 			System.out.print(" or ");
 			System.out.print("Pass" + i + ".WaitTimeout");
 		}
-		System.out.print("\n");
-		
-		System.out.print("E<> Pass0.SeatedTimeout");
+		System.out.print(")\n\n");
+
+		System.out.println("Ziel 2");
+		System.out.print("A[] not (Pass0.SeatedTimeout");
 		for (int i=1; i<noPass; i++) {
 			System.out.print(" or ");
 			System.out.print("Pass" + i + ".SeatedTimeout");
 		}
-		System.out.print("\n");
-		
-		System.out.print("E<> Pass0.SeatsFull");
+		System.out.print(")\n\n");
+
+		System.out.println("Ziel 5");
+		System.out.print("A[] (seats[1] < 7) imply not (Pass0.SeatsFull");
 		for (int i=1; i<noPass; i++) {
 			System.out.print(" or ");
 			System.out.print("Pass" + i + ".SeatsFull");
 		}
-		System.out.print("\n");
+		System.out.print(")\n\n");
 		
-		System.out.print("A<> Pass0.Arrival");
+		System.out.println("Ziel 4");
+		System.out.print("A[] (taxiRounds > 4 and taxiRounds < 6) imply (Pass0.Arrival");
 		for (int i=1; i<noPass; i++) {
 			System.out.print(" and ");
 			System.out.print("Pass" + i + ".Arrival");
 		}
-		System.out.print("\n");
+		System.out.print(")\n");
 	}
 
 }
